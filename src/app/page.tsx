0a82997190662b0dc3a3a69241f63188164481ee
Damian Sobek <sobe66@gmail.com>
2025-06-30 23:18:35 +0200

    Added temperature chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 import { SignOut } from "./components/SignOut/SignOut";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import TemperatureChart from "./components/TemperatureChart/TemperatureChart";
 
 const Page = async () => {
-  const session = await auth();
+  // const session = await auth();
 
-  console.log(session);
+  // console.log(session);
 
-  if (!session) redirect("/sign-in");
+  // if (!session) redirect("/sign-in");
 
   return (
     <>
-      <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
-        <p className="text-gray-600">Signed in as:</p>
-        <p className="font-medium">{session.user?.email}</p>
-      </div>
-
-      <SignOut />
+      <TemperatureChart />
     </>
   );
 };
